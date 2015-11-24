07495e9a7eadccbea9c67eb22ce17752239c1861
paulfrmbrn <lesspaul@bk.ru>
2015-11-24 10:04:25 +0300

    graph implementation

---
+++ src/main/java/ru/mercuriev/game/of/life/graph/Cell.java
@@ -0,0 +1,42 @@
+package ru.mercuriev.game.of.life.graph;
+
+import java.util.stream.IntStream;
+
+/**
+ * works only with cells,
+ * do not know anything about arrays
+ *
+ * @author paul
+ */
+final class Cell {
+
+    private Cell top = null;
+    private Cell bottom = null;
+    private Cell left = null;
+    private Cell right = null;
+
+    /**
+     * initializes current generation of the world
+     */
+    static Cell newInstance(IntStream inputData, int lineLength) {
+        // 1. produce FIRST Cell linked by Cell1.right = Cell2.left with length "lineLength"
+        // 2. produce NEXT Cell linked by Cell1.right = Cell2.left with length "lineLength"
+        // 3. merge FIRST.bottom and NEXT.up cells,
+        // 4. repeat (lineLength - 2) times
+        return null;
+    }
+
+    /**
+     * produces the next generation of World
+     *
+     * @return left top cell pointer
+     */
+    Cell nextGeneration() {
+        // 1. for each cell in world line by line count neighbohoods and decide will it be alive
+        // 2. put values it to the IntStream
+        // 3. call newInstance
+        return null;
+    }
+
+}
+
---
+++ src/main/java/ru/mercuriev/game/of/life/graph/GraphEngine.java
@@ -0,0 +1,22 @@
+package ru.mercuriev.game.of.life.graph;
+
+/**
+ * interface Engine implementation and proxy to the Cells (convert int[][] to Cell)
+ *
+ * @author paul
+ */
+public class GraphEngine {
+
+    /**
+     * interface method implementation
+     */
+    // TODO move method to the interface
+    public int[][] next(int[][] world) {
+        // 1. create one dimension array
+        // 2. get stream of it
+        // 3. pass it and world.length to Cell.next
+        // 4. convert World to int[][]
+        return null;
+    }
+
+}
