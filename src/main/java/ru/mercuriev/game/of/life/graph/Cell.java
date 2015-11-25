b91697268ae6743f078916c1c133ceaf83e95593
paulfrmbrn <lesspaul@bk.ru>
2015-11-25 22:59:46 +0300

    graph implementation.  it is ok to start write tests

--- src/main/java/ru/mercuriev/game/of/life/graph/Cell.java
+++ src/main/java/ru/mercuriev/game/of/life/graph/Cell.java
@@ -11,8 +11,11 @@ import java.util.Objects;
  */
 final class Cell {
 
-    Integer currentState = -1; // 0 - dead, 1 - live. represents current generation
-    Integer nextState = -1; // 0 - dead, 1 - live. represents next generation
+    public static final int DEAD = 0;
+    public static final int ALIVE = 1;
+
+    Integer currentState = 0; // 0 - dead, 1 - live. represents current generation
+    Integer nextState = 0; // 0 - dead, 1 - live. represents next generation
     Cell left = null;
     Cell top = null;
     Cell right = null;
@@ -29,43 +32,98 @@ final class Cell {
         Objects.nonNull(inputData);
 
         return Arrays.stream(inputData)
-              .map(integers -> Arrays.stream(integers)
-                                     .collect(new LineCollector()))
-              .collect(CellHolder::new,
-                       CellHolder::mergeLines,
-                       CellHolder::mergeLines)
-              .toCell()
-              .calculateNextGeneration();
+                .map(integers -> Arrays.stream(integers)
+                        .collect(new LineCollector()))
+                .collect(CellHolder::new,
+                        CellHolder::mergeLines,
+                        CellHolder::mergeLines)
+                .toCell()
+                .calculateNextGeneration();
 
     }
 
     /**
      * calculate the next generation of World
-     *
      */
     private Cell calculateNextGeneration() {
 
         Cell current = this;
 
-        do {
-            current.calculateNextState();
+        while (true) { // TODO bad bad bad rewrite
 
+            // calculate next state for all cells in line
+            current.calculateNextState();
             while (current.right != null) {
-                current.calculateNextState();
                 current = current.right;
+                current.calculateNextState();
             }
 
             // rewind to the left most cell
             while (current.left != null)
                 current = current.left;
 
-        } while ();
+            if (current.bottom != null)
+                current = current.bottom;
+            else {
+                break;
+            }
+
+        }
 
         return this;
 
     }
 
-    private void calculateNextState() {
+    private void calculateNextState() { // TODO bad. rewrite
+
+        int count = getCount();
+        if (this.currentState == ALIVE && (count == 2 || count == 3)) {
+            this.nextState = ALIVE;
+            return;
+        }
+        if (this.currentState == DEAD && count == 3) {
+            this.nextState = ALIVE;
+            return;
+        }
+        this.nextState = DEAD;
+
+    }
+
+    private int getCount() {  // TODO. things planned to be simple look like shit. rewrite
+
+        int count = 0;
+
+        if (this.top != null) {
+
+            count += this.top.currentState;
+
+            if (this.top.left != null)
+                count += this.top.left.currentState;
+
+            if (this.top.right != null)
+                count += this.top.right.currentState;
+        }
+
+        if (this.bottom != null) {
+
+            count += this.bottom.currentState;
+
+            if (this.bottom.left != null)
+                count += this.bottom.left.currentState;
+
+            if (this.bottom.right != null)
+                count += this.bottom.right.currentState;
+        }
+
+        if (this.left != null) {
+            count += this.left.currentState;
+        }
+
+        if (this.right != null) {
+            count += this.right.currentState;
+        }
+
+        return count;
 
     }
 
