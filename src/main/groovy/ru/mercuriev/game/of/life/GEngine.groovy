d77b4e73ad2722aa8d921b8e1c452955e3f7301e
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2015-12-17 02:02:25 +0300

    groovy first attempt classes add

---
+++ src/main/groovy/ru/mercuriev/game/of/life/GEngine.groovy
@@ -0,0 +1,41 @@
+package ru.mercuriev.game.of.life
+
+import org.springframework.stereotype.Service
+import ru.mercuriev.game.of.life.NeighboursCounter
+
+import java.util.stream.IntStream
+
+/**
+ * Created by Eugene on 17.12.15.
+ */
+@Service
+class GEngine {
+    public static final int ALIVE = 1;
+    public static final int DEAD = 0;
+
+    public int[][] next(int[][] world) {
+
+        int size = world.length;
+        int[][] result = new int[size][size];
+
+        int[][] cellNeighborsAmount = NeighboursCounter.newInstance(world).countNeighbours();
+
+        IntStream.range(0, size).eachWithIndex { i ->
+            IntStream.range(0, size).eachWithIndex { j ->
+                result[i][j] = nextState(world[i][j], cellNeighborsAmount[i][j])
+            }
+        }
+        return result;
+    }
+
+    protected int nextState(int currentState, int neighboursCount) {
+        if (currentState == ALIVE) {
+            return (neighboursCount < 2 || neighboursCount > 3) ? DEAD : ALIVE;
+        }
+        if (currentState == DEAD && neighboursCount == 3) {
+            return ALIVE;
+        }
+        return currentState;
+    }
+
+}
---
+++ src/main/groovy/ru/mercuriev/game/of/life/GNeighboursCounter.groovy
@@ -0,0 +1,55 @@
+package ru.mercuriev.game.of.life
+
+import java.util.stream.IntStream
+
+/**
+ * Created by Eugene on 17.12.15.
+ */
+class GNeighboursCounter {
+
+    private int[][] world;
+
+    public static GNeighboursCounter newInstance(int[][] world) {
+        return new GNeighboursCounter(world);
+    }
+
+    private NeighboursCounter(int[][] world) {
+        this.world = world;
+    }
+
+    public int[][] countNeighbours() {
+
+        int size = world.length;
+        int[][] result = new int[size][size];
+
+        IntStream.range(0, size).eachWithIndex { i ->
+            IntStream.range(0, size).eachWithIndex { j ->
+                result[i][j] = this.cellNeighborsAmount(i, j)
+            }
+        };
+        return result;
+    }
+
+    private int cellNeighborsAmount(int i, int j) {
+        int neighboursCount = 0;
+        for (int x = i - 1; x <= i + 1; x++) {
+            for (int y = j - 1; y <= j + 1; y++) {
+                if (isOutOfBorder(x, y)) {
+                    continue;
+                }
+                // with no impact of the cell itself
+                if (x == i && y == j) {
+                    continue;
+                }
+                neighboursCount += world[x][y];
+            }
+        }
+        return neighboursCount;
+    }
+
+    // checking borders to avoid out of range exception
+    private boolean isOutOfBorder(int x, int y) {
+        return x < 0 || x >= world.length || y < 0 || y >= world.length;
+    }
+
+}
